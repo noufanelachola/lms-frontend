eb8272ddb14eb5d5258eaa8a000abbef4c1c4c94
noufanelachola <noufansadiqelachola@gmail.com>
2024-03-27 16:45:21 +0530

    Login completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import Login from './Container/Login/Login';
 
 import './App.css';
 import React from 'react';
 
 function App() {
+  const [route,setRoute] = useState("logIn");
+
+  const routeChange = (rout) => {
+    setRoute(rout);
+    console.log(route);
+  }
+
   return (
     <div className="App">
-      <Login/>
+      <Login routeChange={routeChange} />
     </div>
   );
 }
